edf0847a0484eba662db390ccefce0339a6613e7
saurabh khaparey <saurabhkhaparey05@gmail.com>
2025-08-06 11:03:10 +0530

    6/8 practice

--- Arrays/practice.cpp
+++ Arrays/practice.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//PS: Count digits
 // TC: O(log10^N + 1)
 // SC: O(1)
 // Source: TUF
@@ -27,10 +28,75 @@ int countOfDigitsOptimized(int n) {
     return (int)(log10(n) + 1);
 }
 
+//PS: Reverse number
+// TC: O(log10^N + 1)
+// SC: O(1)
+// Source: TUF
+int reverseNumber(int n) {
+    int rev_num = 0;
+    int num = n;
+
+    while(num > 0) {
+        int rem = num % 10;
+        rev_num = rev_num * 10 + rem;
+        num = num/10;
+    }
+
+    return rev_num;
+}
+
+//PS: Check if number is palindrome
+// TC: O(log10^N + 1)
+// SC: O(1)
+// Source: TUF
+bool isPalindrome(int n) {
+    return reverseNumber(n) == n;
+}
+
+//PS: GCD of 2 numbers
+// TC: O(min(N1, N2))
+// SC: O(1)
+// Source: TUF
+int findGCD(int a, int b) {
+    int result = min(a, b);
+
+    while(result > 0) {
+        if((a % result == 0) && (b % result == 0)) {
+            break;
+        }
+
+        result--;
+    }
+
+    return result;
+}
+
+//PS: GCD of 2 numbers
+// TC: O(log(min(N1, N2)))
+// SC: O(1)
+// Source: TUF
+int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+
 int main() {
     
-    cout<<countOfDigits(211000)<<endl;
-    cout<<countOfDigitsOptimized(211000);
+    //cout<<countOfDigits(211000)<<endl;
+    //cout<<countOfDigitsOptimized(211000);
+
+    // cout<<reverseNumber(1002345)<<endl;
+    // cout<<reverseNumber(76567575)<<endl;
+    // cout<<reverseNumber(54312);
+
+    // cout<<isPalindrome(1234)<<endl;
+    // cout<<isPalindrome(243423)<<endl;
+    // cout<<isPalindrome(441144)<<endl;
+    // cout<<isPalindrome(111111);
+
+    // cout<<gcd(20, 28)<<endl;
+    // cout<<gcd(20, 15)<<endl;
+    // cout<<gcd(13, 17);
 
     return 0;
 }
