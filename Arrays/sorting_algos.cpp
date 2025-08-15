2c22dc9825b12d9b75120876f6847a2bca7fbbb4
saurabh khaparey <saurabhkhaparey05@gmail.com>
2025-08-15 21:34:52 +0530

    sorting techniques

--- Arrays/sorting_algos.cpp
+++ Arrays/sorting_algos.cpp
@@ -60,6 +60,18 @@ void bubble_sort(int arr[], int n) {
     print_arr(arr, n);
 }
 
+void bubble_sort_recursive(int arr[], int index, int n) {
+    if(index == n-1) return;
+
+    for(int j=0;j<n-index-1;j++) {
+        if(arr[j] > arr[j+1]) {
+            swap(arr[j], arr[j+1]);
+        }
+    }
+
+    bubble_sort_recursive(arr, index+1, n);
+}
+
 /*
 TC = O(n*n), worst case
 TC = O(n) - best case if the array is alread sorted
@@ -80,15 +92,91 @@ void insertion_sort(int arr[], int n) {
     print_arr(arr, n);
 }
 
+void merge(int arr[], int start, int mid, int end) {
+    int len = end-start+1;
+    int temp[len] = {0};
+    int i = start;
+    int j = mid+1;
+    int k = 0;
+    while(i <= mid && j <= end) {
+        if(arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
+        } else {
+            temp[k++] = arr[j++];
+        }
+    }
+
+    while(i <= mid) {
+        temp[k++] = arr[i++];
+    }
+
+    while(j <= end) {
+        temp[k++] = arr[j++];
+    }
+
+    for(int i=0;i<len;i++) arr[i+start] = temp[i];
+}
+
+void mergeSort(int arr[], int start, int end) {
+    if(start >= end) return;
+
+    int mid = (start + end)/2;
+
+    mergeSort(arr, start, mid);
+    mergeSort(arr, mid+1, end);
+    merge(arr, start, mid, end);
+}
+
+/*
+TC = O(nlog2n)
+SC = O(n), temporary array for merging
+*/
+void merge_sort(int arr[], int n) {
+    mergeSort(arr, 0, n-1);    
+    print_arr(arr, n);
+}
+
+
+int getPartitionIndex(int arr[], int low, int high) {
+    int pivot = low;
+    int i = low;
+    int j = high;
+
+    while(i < j) {
+        while(i <= high && arr[i] <= arr[pivot]) i++;
+        while(j >= low && arr[j] > arr[pivot]) j--;
+        if(i < j) swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+
+    return j;
+}
+
+/*
+TC = O(nlog2n)
+TC = O(n*n), worst case when the array is sorted and the pivot is the first/last element
+this results in skewed recursion tree making the time complexity quadratic
+SC = O(1), temporary array for merging
+*/
+void quick_sort(int arr[], int start, int end) {
+    if(start < end) {
+        int PI = getPartitionIndex(arr, start, end);
+        quick_sort(arr, start, PI-1);
+        quick_sort(arr, PI+1, end);
+    }
+}
+
 int main() {
     int arr[] = {9, -1, -11, 1, 8, 8, 7, 2, 3, 6, 0, 5, 4};
     int n = sizeof(arr)/sizeof(arr[0]);
 
     // selection_sort(arr, n);
     // bubble_sort(arr, n);
-    insertion_sort(arr, n);
-    // quick_sort(arr, n);
+    // bubble_sort_recursive(arr, 0, n);
+    // insertion_sort(arr, n);
+    // quick_sort(arr, 0, n-1);
     // merge_sort(arr, n);
+    // print_arr(arr, n);
 
     return 0;
 }
