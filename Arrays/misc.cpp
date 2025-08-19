c19edce782d69ce6a5c72b679589fbc1a2817e14
saurabh khaparey <saurabhkhaparey05@gmail.com>
2025-08-19 23:25:46 +0530

    array problems

--- Arrays/misc.cpp
+++ Arrays/misc.cpp
@@ -5,7 +5,9 @@ This file has some miscelleneous array questions
 - Find second smallest and second largest element in array
 - Check if array is sorted
 - Check if array sorted and rotated
-- Remove duplicated from sorted array
+- Remove duplicates from sorted array
+- Move zeroes to end
+- Union of Two sorted arrays
 */
 
 #include<bits/stdc++.h>
@@ -147,9 +149,57 @@ void leftRotateBy1(int arr[], int n) {
     print_arr(arr, n);
 }
 
+void moveAllZeroesToEnd(int arr[], int n) {
+    int i=0, j=0;
+
+    while(j < n) {
+        while(j < n && arr[j] == 0) j++;
+
+        if(j >= n) break;
+
+        swap(arr[j], arr[i]);
+        i++;
+        j++;
+    }
+
+    print_arr(arr, n);
+}
+
+void unionOfTwoSortedArrays(int arr1[], int arr2[], int n, int m) {
+
+    vector<int> unionArray = {};
+    int i=0, j=0;
+
+    while(i < n && j < m) {
+        if(arr1[i] <= arr2[j]) {
+            if(unionArray.size() == 0 || unionArray.back() != arr1[i]) 
+                unionArray.push_back(arr1[i]);
+            i++;
+        } else {
+            if(unionArray.size() == 0 || unionArray.back() != arr2[j])
+                unionArray.push_back(arr2[j]);
+            j++;
+        }
+    }
+
+    while(i < n) {
+        if(unionArray.back() != arr1[i])
+            unionArray.push_back(arr1[i]);
+        i++;
+    }
+
+    while(j < m) {
+        if(unionArray.back() != arr2[j])
+            unionArray.push_back(arr2[j]);
+        j++;
+    }
+   
+    for(auto x: unionArray) cout<<x<<" ";
+}
+
 int main() {
-    int arr[] = {1, 2, 3, 4, 5};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    //int arr[] = {0, 0, 0, 0, 1, 1, 1};
+    //int n = sizeof(arr)/sizeof(arr[0]);
     
     //largestElementInArray(arr, n);
     
@@ -164,5 +214,11 @@ int main() {
     //vector<int> nums = {2,7,4,1,2,6,2};
     //cout<<check(nums);
 
-    leftRotateBy1(arr, n);
+    //leftRotateBy1(arr, n);
+
+   //moveAllZeroesToEnd(arr, n);
+    int n = 10, m = 7;
+    int arr1[] = {1,2,3,4,5,6,7,8,9,10}; 
+    int arr2[] = {2,3,4,4,5,11,12};
+    unionOfTwoSortedArrays(arr1, arr2, n, m);
 }
