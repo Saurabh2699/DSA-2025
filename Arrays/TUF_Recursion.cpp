b2ec94988e1189ea91f3050d5d719c91601d93d5
saurabh khaparey <saurabhkhaparey05@gmail.com>
2025-10-08 23:21:27 +0530

    8/10/25

--- Arrays/TUF_Recursion.cpp
+++ Arrays/TUF_Recursion.cpp
@@ -1,28 +1,40 @@
-#/**************************************************************
-# TUF_Recursion.cpp: Classic Recursion Problems and Solutions
-# ------------------------------------------------------------
-# This file contains C++ implementations of basic recursion problems:
-#   1. printAllDivisors: Prints all divisors of a number
-#   2. printNto1: Prints numbers from N to 1
-#   3. sumOfN: Returns sum of first N natural numbers
-#   4. reverseArray: Recursively reverses an array
-#   5. reverseArrayIterative: Iteratively reverses an array
-#   6. checkPallindrome: Checks if a string is a palindrome using recursion
-# ------------------------------------------------------------
-# Time and Space Complexity:
-#   - printAllDivisors:      Time O(sqrt(N)), Space O(sqrt(N))
-#   - printNto1:             Time O(N), Space O(N)
-#   - sumOfN:                Time O(N), Space O(N)
-#   - reverseArray:          Time O(N), Space O(N)
-#   - reverseArrayIterative: Time O(N), Space O(1)
-#   - checkPallindrome:      Time O(N), Space O(N)
-# ------------------------------------------------------------
-# Author: Saurabh2699
-# Last Updated: October 2, 2025
-#**************************************************************/
+/**************************************************************
+* TUF_Recursion.cpp: Classic Recursion Problems and Solutions
+* ------------------------------------------------------------
+* This file contains C++ implementations of basic recursion problems:
+*   1. printAllDivisors: Prints all divisors of a number
+*   2. printNto1: Prints numbers from N to 1
+*   3. sumOfN: Returns sum of first N natural numbers
+*   4. reverseArray: Recursively reverses an array
+*   5. reverseArrayIterative: Iteratively reverses an array
+*   6. checkPallindrome: Checks if a string is a palindrome using recursion
+* ------------------------------------------------------------
+* Related LeetCode Links:
+*   - Print All Divisors: https://leetcode.com/problems/find-all-divisors-of-a-number/ (similar)
+*   - Print N to 1: https://leetcode.com/problems/print-numbers-by-recursion/ (similar)
+*   - Sum of N: https://leetcode.com/problems/sum-of-numbers/ (similar)
+*   - Reverse Array: https://leetcode.com/problems/reverse-string/ (for string, similar logic)
+*   - Check Palindrome: https://leetcode.com/problems/valid-palindrome/
+* ------------------------------------------------------------
+* Time and Space Complexity:
+*   - printAllDivisors:      Time O(sqrt(N)), Space O(sqrt(N))
+*   - printNto1:             Time O(N), Space O(N)
+*   - sumOfN:                Time O(N), Space O(N)
+*   - reverseArray:          Time O(N), Space O(N)
+*   - reverseArrayIterative: Time O(N), Space O(1)
+*   - checkPallindrome:      Time O(N), Space O(N)
+* ------------------------------------------------------------
+* Author: Saurabh2699
+* Last Updated: October 5, 2025
+**************************************************************/
 #include<bits/stdc++.h>
 using namespace std;
 
+/**
+ * Prints all divisors of a given number n.
+ * Similar: https://leetcode.com/problems/find-all-divisors-of-a-number/
+ * Time Complexity: O(sqrt(n)), Space Complexity: O(sqrt(n))
+ */
 void printAllDivisors(int n) {
     vector<int> v;
     
@@ -40,6 +52,11 @@ void printAllDivisors(int n) {
     for(auto x: v) cout<<x<<" ";
 }
 
+/**
+ * Prints numbers from n down to 1 using recursion.
+ * Similar: https://leetcode.com/problems/print-numbers-by-recursion/
+ * Time Complexity: O(n), Space Complexity: O(n)
+ */
 void printNto1(int n) {
     if(n == 0) return;
 
@@ -48,12 +65,22 @@ void printNto1(int n) {
     printNto1(n-1);
 }
 
+/**
+ * Returns the sum of first n natural numbers using recursion.
+ * Similar: https://leetcode.com/problems/sum-of-numbers/
+ * Time Complexity: O(n), Space Complexity: O(n)
+ */
 int sumOfN(int n) {
     if(n == 0) return 0;
 
     return n + sumOfN(n-1);
 }
 
+/**
+ * Recursively reverses an array in place.
+ * Similar logic: https://leetcode.com/problems/reverse-string/
+ * Time Complexity: O(n), Space Complexity: O(n)
+ */
 void reverseArray(int arr[], int n, int index) {
    if(index == n/2) return;
    
@@ -66,6 +93,10 @@ void reverseArray(int arr[], int n, int index) {
    reverseArray(arr, n, index+1);
 }
 
+/**
+ * Iteratively reverses an array in place.
+ * Time Complexity: O(n), Space Complexity: O(1)
+ */
 void reverseArrayIterative(int arr[], int n) {
     int i=0, j=n-1;
 
@@ -76,6 +107,11 @@ void reverseArrayIterative(int arr[], int n) {
     }
 }
 
+/**
+ * Recursively checks if a string is a palindrome.
+ * LeetCode: https://leetcode.com/problems/valid-palindrome/
+ * Time Complexity: O(n), Space Complexity: O(n)
+ */
 bool checkPallindrome(string s, int i) {
     int len = s.length();
 
@@ -86,6 +122,9 @@ bool checkPallindrome(string s, int i) {
     return checkPallindrome(s, i+1);
 }
  
+/**
+ * Main function to run sample test cases for recursion problems.
+ */
 int main() {
     //printAllDivisors(36);
     //printNto1(10);
--- Binary Search/BS_TUF_BS_On_Answers.cpp
+++ Binary Search/BS_TUF_BS_On_Answers.cpp
@@ -10,6 +10,8 @@
 *   5. Smallest Divisor Given a Threshold
 *   6. Capacity To Ship Packages Within D Days
 *   7. Kth missing positive number
+*   8. Aggresive Cows
+*   9. Split Array Largest Sum
 * ------------------------------------------------------------
 * Sources & Problem Links:
 *   1. Square Root: https://leetcode.com/problems/sqrtx/
@@ -17,6 +19,10 @@
 *   3. Koko Eating Bananas: https://leetcode.com/problems/koko-eating-bananas/
 *   4. Minimum Days to Make Bouquets: https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets/
 *   5. Smallest Divisor: https://leetcode.com/problems/find-the-smallest-divisor-given-a-threshold/
+*   6. Capacity To Ship Packages Within D Days: https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
+*   7. Kth Missing Positive Number: https://leetcode.com/problems/kth-missing-positive-number/
+*   8. Aggressive Cows: https://www.spoj.com/problems/AGGRCOW/ (SPOJ)
+*   9. Split Array Largest Sum: https://leetcode.com/problems/split-array-largest-sum/
 * ------------------------------------------------------------
 * Author: Saurabh2699
 * Last Updated: October 2, 2025
@@ -236,6 +242,12 @@ int smallestDivisor(vector<int>& nums, int threshold) {
     return ans;
 }
 
+/**
+ * Helper function for Capacity To Ship Packages Within D Days problem.
+ * Calculates the number of days needed to ship all packages with given capacity.
+ * Time Complexity: O(N)
+ * Space Complexity: O(1)
+ */
 int getDaysToShip(vector<int> &weights, int capacity) {
     int currCapacity = 0;
     int days = 1;
@@ -252,6 +264,12 @@ int getDaysToShip(vector<int> &weights, int capacity) {
     return days;
 }
 
+/**
+ * Finds the minimum capacity to ship packages within given days using binary search.
+ * LeetCode: https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/
+ * Time Complexity: O(N log S), S = sum of weights
+ * Space Complexity: O(1)
+ */
 int shipWithinDays(vector<int>& weights, int days) {
     int n = weights.size();
     int maxi = INT_MIN;
@@ -280,7 +298,13 @@ int shipWithinDays(vector<int>& weights, int days) {
     return ans;
 }
 
- int findKthPositive(vector<int>& arr, int k) {
+/**
+ * Finds the kth missing positive number in a sorted array using binary search.
+ * LeetCode: https://leetcode.com/problems/kth-missing-positive-number/
+ * Time Complexity: O(log N)
+ * Space Complexity: O(1)
+ */
+int findKthPositive(vector<int>& arr, int k) {
     // Brute Force Approach
     // int n = arr.size();
 
@@ -318,9 +342,120 @@ int shipWithinDays(vector<int>& weights, int days) {
 }
 
 /**
+ * Helper function for Aggressive Cows problem.
+ * Counts how many cows can be placed with at least 'distance' apart.
+ * Time Complexity: O(N)
+ * Space Complexity: O(1)
+ */
+int countCows(vector<int> &stalls, int distance) {
+    int cows = 1;
+    int prev = stalls[0];
+
+    for(int i=1; i<stalls.size(); i++) {
+        int diff = stalls[i] - prev;
+        if(diff >= distance) {
+            cows++;
+            prev = stalls[i];
+        }
+    }
+
+    return cows;
+}
+
+/**
+ * Finds the largest minimum distance to place k cows in stalls using binary search.
+ * SPOJ: https://www.spoj.com/problems/AGGRCOW/
+ * Time Complexity: O(N log D), D = stalls[n-1] - stalls[0]
+ * Space Complexity: O(1)
+ */
+int agressiveCows(vector<int> &stalls, int k) {
+    int n = stalls.size();
+
+    sort(stalls.begin(), stalls.end());
+
+    int low = 1;
+    int high = stalls[n-1]-stalls[0];
+    int ans = 1;
+
+    while(low <= high) {
+        int mid = (low + high)/2;
+        int cows = countCows(stalls, mid);
+        if(cows >= k) {
+            ans = mid;
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return ans;
+}
+
+
+/**
+ * Helper function for Split Array Largest Sum problem.
+ * Counts how many subarrays are needed if the maximum allowed sum is 'sum'.
+ * Time Complexity: O(N)
+ * Space Complexity: O(1)
+ */
+int getSubArraysCount(vector<int> &nums, int sum) {
+    int curr_sum = 0;
+    int subarrays = 1;
+
+    for(int i=0; i<nums.size(); i++) {
+        if(curr_sum + nums[i] > sum) {
+            subarrays++;
+            curr_sum = nums[i];
+        } else {
+            curr_sum += nums[i];
+        }
+    }
+
+    return subarrays;
+}
+
+/**
+ * Splits the array into k subarrays such that the largest sum among them is minimized.
+ * LeetCode: https://leetcode.com/problems/split-array-largest-sum/
+ * Time Complexity: O(N log S), S = sum of array elements
+ * Space Complexity: O(1)
+ */
+int splitArray(vector<int>& nums, int k) {
+    int n = nums.size();
+    int sum = 0;
+    int maxi = INT_MIN;
+
+    for(int x: nums) {
+        maxi = max(maxi, x);
+        sum += x;
+    }
+
+    int low = maxi;
+    int high = sum;
+    int ans = maxi;
+
+    while(low <= high) {
+        int mid = (low + high)/2;
+        int subarrays = getSubArraysCount(nums, mid);
+
+        if(subarrays <= k) {
+            ans = mid;
+            high = mid-1;
+        } else {
+            low = mid+1;
+        }
+    }
+
+    return ans;
+}
+
+/**
  * Main function to run sample test cases and demonstrate binary search solutions.
  */
 int main() {
     //cout<<squareRoot(89);
-    cout<<findNthRoot(3, 8);
+    //cout<<findNthRoot(3, 8);
+
+    vector<int> v = {4,2,1,3,6};
+    cout<<agressiveCows(v, 2);
 }
